c6e6d217307d0a2276e19c6eae68304572898e53
Alice <alicewc95@gmail.com>
2018-04-05 10:25:21 -0700

    hide old tasks

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,7 +1,30 @@
 (function() {
   function HomeCtrl(Task) {
     this.tasks = Task.all;
-  };
+
+
+    this.hide = function(task) {
+     return task.created < (moment().dayOfYear() - 7) || task.completed == true
+    };
+
+    this.addTask = function () {
+      if (this.title) {
+        this.task.$add({
+          title: this.title,
+          description: this.description,
+          created: moment().dayOfYear(),
+          completed: this.completed
+        });
+        this.title = '';
+      }
+    };
+
+    this.expiredTask = function (created) {
+      if (created < Date.now() - 604800000) {
+        return true;
+      }
+    }
+  }
 
   angular
     .module('blocitoff')
